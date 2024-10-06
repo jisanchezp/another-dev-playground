13761f10c48ac9343f7b0ec8a344cd35da2a5d4d
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-06 17:44:32 -0300

    Added array initializations and comments

--- AnotherDevPlayground.Lib/Playgrounds/DataTypePlaygrounds/ArrayPlayground.cs
+++ AnotherDevPlayground.Lib/Playgrounds/DataTypePlaygrounds/ArrayPlayground.cs
@@ -30,7 +30,8 @@ namespace AnotherDevPlayground.Lib.Playgrounds.DataTypePlaygrounds
         public static void ArrayInAllDimensions() 
         {
             // Declare a single-dimensional array of 5 integers.
-            int[] array1 = new int[1];
+            int[] array1 = new int[10]; // Elements are zero initialized
+            string[] array1b = new string[5]; // Elements are null initialized
 
             // Declare and set array element values.
             int[] array2 = [1, 2, 3, 4, 5, 6]; // Collection expression sintax. C# 12+ (.NET 8).
@@ -44,9 +45,13 @@ namespace AnotherDevPlayground.Lib.Playgrounds.DataTypePlaygrounds
 
             // Declare a jagged array.
             int[][] jaggedArray = new int[6][];
+            int[][] jaggedArray2 = new int[6][];
 
             // Set the values of the first array in the jagged array structure.
             jaggedArray[0] = [1, 2, 3, 4];
+
+            jaggedArray2[0] = array1;
+            jaggedArray2[1] = array2;
         }        
     }
 }
