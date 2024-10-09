c367cfc093829d5126bb73eb0d0167871e0404bc
Juan I. Sánchez <juanisanchez@outlook.com>
2024-10-09 00:33:40 -0300

    Refactored player movement game code
    Moved logic from PlayGround program to their
    own clases.

--- AnotherDevPlayground.Lib/PlaygroundProgram.cs
+++ AnotherDevPlayground.Lib/PlaygroundProgram.cs
@@ -1,8 +1,10 @@
 ﻿using AnotherDevPlayground.Lib.Playgrounds.DataTypePlaygrounds;
+using AnotherDevPlayground.Lib.Playgrounds.Game.PlayerMovementGame;
 using AnotherDevPlayground.Models.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
@@ -24,52 +26,12 @@ namespace AnotherDevPlayground.Lib
                 Name = "Playerito"
             };
 
-            int[] map = new int[64];
-            player.Position.X = Random.Shared.Next(100);
-            map[player.Position.X] = (int) MapObjectEnum.Player;
+            int mapSize = 64;
+            var playerMovementGame = new PlayerMovementGame(player, mapSize);
+            playerMovementGame.Start();
 
-            MoveLeft(map, player);
-            MoveRight(map, player);
-            ArrayPlayground.PassSingleDimensionalArraysAsArguments();
-
-            ArrayPlayground.ArrayInAllDimensions();
+            //ArrayPlayground.PassSingleDimensionalArraysAsArguments();
+            //ArrayPlayground.ArrayInAllDimensions();
         }
-
-        private static void MoveRight(int[] map, Player player)
-        {
-            bool output = false;
-
-            if (player.Position.X < map.Length - 1)
-            {
-                map[player.Position.X] = (int)MapObjectEnum.Empty;
-                player.Position.X += 1;
-                map[player.Position.X] = (int)MapObjectEnum.Player;
-                output = true;
-            }
-
-            return output;
-        }
-
-        private static bool MoveLeft(int[] map, Player player)
-        {
-            bool output = false;
-
-            if (player.Position.X > 0)
-            {
-                map[player.Position.X] = (int)MapObjectEnum.Empty;
-                player.Position.X -= 1;
-                map[player.Position.X] = (int)MapObjectEnum.Player;
-                output = true;
-            }
-
-            return output;
-        }
-    }
-
-    internal enum MapObjectEnum : int
-    {
-        Empty = 0,
-        Player = 1,
-        Enemy = 2,
     }
 }
---
+++ AnotherDevPlayground.Lib/Playgrounds/Game/Player/PlayerMovementGame.cs
@@ -0,0 +1,94 @@
+﻿using AnotherDevPlayground.Lib.Playgrounds.DataTypePlaygrounds;
+using AnotherDevPlayground.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherDevPlayground.Lib.Playgrounds.Game.PlayerMovementGame
+{
+
+    internal class PlayerMovementGame
+    {
+        private int[] _map;
+        private Player _player;
+
+        public PlayerMovementGame(Player player, int mapSize)
+        {
+            _player = player;
+            _map = new int[mapSize];
+        }
+        public void Start()
+        {
+            _player.Position.X = Random.Shared.Next(100);
+            _map[_player.Position.X] = (int)MapObjectEnum.Player;
+            MoveLeftWithLog();
+            MoveLeftWithLog();
+            MoveLeftWithLog();
+            MoveRightWithLog();
+            MoveRightWithLog();
+            MoveRightWithLog();
+            MoveLeftWithLog();
+            MoveLeftWithLog();
+        }
+
+
+        private void MoveLeftWithLog()
+        {
+            var hasPlayerMoved = MoveLeft();
+            LogPlayerMovement(hasPlayerMoved, "left");
+        }
+
+        private void MoveRightWithLog()
+        {
+            var hasPlayerMoved = MoveRight();
+            LogPlayerMovement(hasPlayerMoved, "right");
+        }
+
+        private void LogPlayerMovement(bool hasPlayerMoved, string direction)
+        {
+            if (hasPlayerMoved)
+                Console.WriteLine($"Player moved {direction} successfully. Current position: {_player.Position.X}");
+        }
+
+        private bool MoveRight()
+        {
+            bool output = false;
+
+            if (_player.Position.X < _map.Length - 1)
+            {
+                _map[_player.Position.X] = (int)MapObjectEnum.Empty;
+                _player.Position.X += 1;
+                _map[_player.Position.X] = (int)MapObjectEnum.Player;
+                output = true;
+            }
+
+            return output;
+        }
+
+        private bool MoveLeft()
+        {
+            bool output = false;
+
+            if (_player.Position.X > 0)
+            {
+                _map[_player.Position.X] = (int)MapObjectEnum.Empty;
+                _player.Position.X -= 1;
+                _map[_player.Position.X] = (int)MapObjectEnum.Player;
+                output = true;
+            }
+
+            return output;
+        }
+
+
+        internal enum MapObjectEnum : int
+        {
+            Empty = 0,
+            Player = 1,
+            Enemy = 2,
+        }
+    }
+}
